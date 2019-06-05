8579419405ee5ce2964045a9bfe541c8317d967a
Brandon Campbell <brandon@yinzcam.com>
2019-06-05 17:59:25 -0400

    Initial Commit

---
+++ codemash/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  codemash
+//
+//  Created by Brandon Campbell on 6/5/19.
+//  Copyright © 2019 Brandon Campbell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
